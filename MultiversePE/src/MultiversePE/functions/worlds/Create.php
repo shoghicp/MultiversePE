4ca68e40c1e037b133988d196426b1b6d6577b23
Philip Shilling <comedyman937@yahoo.com>
2014-07-15 22:48:16 -0700

    Create Create.php

---
+++ MultiversePE/src/MultiversePE/functions/worlds/Create.php
@@ -0,0 +1,17 @@
+<?php
+namespace MultiversePE\functions\worlds;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Player;
+
+class Create extends PluginTask{
+  private $p;
+  public function onRun(){
+  }
+  
+  public function createWorld($name){
+    $this->name = $name;
+    //TODO: Generate new world with name $this->name
+  }
+}
+?>
