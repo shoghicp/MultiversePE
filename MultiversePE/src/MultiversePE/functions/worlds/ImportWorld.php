9b79b1175436c0ee4554e5c55689468f8d798950
Philip Shilling <comedyman937@yahoo.com>
2014-07-15 23:03:18 -0700

    Update and rename Import.php to ImportWorld.php

--- MultiversePE/src/MultiversePE/functions/worlds/ImportWorld.php
+++ MultiversePE/src/MultiversePE/functions/worlds/ImportWorld.php
@@ -4,7 +4,7 @@ namespace MultiversePE\functions\worlds;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Player;
 
-class Import extends PluginTask{
+class ImportWorld extends PluginTask{
   public function onRun(){
   }
   
