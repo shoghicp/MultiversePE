7ea1369bfb1f2a587a6180db8203ed107f249262
Philip Shilling <comedyman937@yahoo.com>
2014-07-15 23:02:15 -0700

    Update and rename Create.php to CreateWorld.php

--- MultiversePE/src/MultiversePE/functions/worlds/CreateWorld.php
+++ MultiversePE/src/MultiversePE/functions/worlds/CreateWorld.php
@@ -4,7 +4,7 @@ namespace MultiversePE\functions\worlds;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Player;
 
-class Create extends PluginTask{
+class CreateWorld extends PluginTask{
   public function onRun(){
   }
   
