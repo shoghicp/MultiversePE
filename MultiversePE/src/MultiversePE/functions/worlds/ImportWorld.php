ebd64bcd5a1a09fd008e72b91e1ba1601e64bb0b
Philip Shilling <comedyman937@yahoo.com>
2014-07-15 23:28:01 -0700

    Import worlds

--- MultiversePE/src/MultiversePE/functions/worlds/ImportWorld.php
+++ MultiversePE/src/MultiversePE/functions/worlds/ImportWorld.php
@@ -2,6 +2,7 @@
 namespace MultiversePE\functions\worlds;
 
 use pocketmine\scheduler\PluginTask;
+use pocketmine\level\LevelImport;
 use pocketmine\Player;
 
 class ImportWorld extends PluginTask{
@@ -10,7 +11,7 @@ class ImportWorld extends PluginTask{
   
   public function importWorld($name){
     $this->name = $name;
-    //TODO: Import new world with name $this->name
+    $this->level->import($this->name); //Not sure how to do this yet
   }
 }
 ?>
